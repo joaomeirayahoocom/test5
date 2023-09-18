3e5eb499f403a6dfee3c88dd8e1afb4e6907dbbe
JOAO MEIRA <joaogmn@yahoo.com>
2023-09-18 09:25:30 -0700

    first

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            ViewData["EnvName"] = _configuration["EnvironmentName"];
+            ViewData["Level2Setting"] = _configuration["Level1:Level2"];
+            ViewData["NonStickySetting"] = _configuration["NonStickySetting"];
+            ViewData["ConnectionString"] = _configuration.GetConnectionString("MyDBConnection");
+        }
+    }
+}
+
